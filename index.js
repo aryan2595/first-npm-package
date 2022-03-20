50cc0caa957559bdb478fcc028344af76aea7db9
Aryan Raj <aryan251195@gmail.com>
2022-03-21 00:48:25 +0530

    bug fixed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const os = require("os");
 function isLoggedIn() {
+	return path.join(os.path.realpath(__file__));
 	return os.path.dirname(os.path.realpath(__file__));
-	return path.join(__dirname);
 }
 
 module.exports = isLoggedIn;
